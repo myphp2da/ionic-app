42633451ed00f7c252b002cb2974bb8333452b5d
myphp2da <45788222+myphp2da@users.noreply.github.com>
2019-02-21 21:31:27 +0530

    Delete email-config.php

--- include/email-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('MAIL_HOST', 'mail.innocon.in');
-    define('MAIL_PORT', '587');
-    define('MAIL_FROM', 'noreply@innocon.in');
-    define('MAIL_PASSWORD', 'pcFPdTBu');
-    define('MAIL_SECURE', 'tls');
-    define('MAIL_NAME', SITE_TITLE);
